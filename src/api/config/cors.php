f50d5a6e8b5d9c39afa4f2f6cdc95903a5ce4282
Jesus Manuel Bobadilla <aldinhobobadilla@gmail.com>
2025-11-20 14:22:12 -0600

    Correcion de cors

--- src/api/config/cors.php
+++ src/api/config/cors.php
@@ -3,15 +3,15 @@
 
 // Permitir solicitudes desde cualquier origen
 header("Access-Control-Allow-Origin: *");
-
-// Permitir métodos
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-
-// Permitir headers personalizados
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+// Permitir métodos GET y POST
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+// Permitir headers personalizados si los necesitas
+header("Access-Control-Allow-Headers: Content-Type");
 
 // Si es OPTIONS, responder vacío y terminar (evita errores preflight)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
+
+
--- src/api/routes/reports/getReports.php
+++ src/api/routes/reports/getReports.php
@@ -1,6 +1,13 @@
 <?php
 require "../../core/db.php";
-require_once __DIR__ . "../../config/cors.php";
+// require_once __DIR__ . "../../config/cors.php";
+
+// Permitir solicitudes desde cualquier origen
+header("Access-Control-Allow-Origin: *");
+// Permitir métodos GET y POST
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+// Permitir headers personalizados si los necesitas
+header("Access-Control-Allow-Headers: Content-Type");
 
 $db = new DB();
 $data = $db->query("SELECT * FROM reportes")->fetchAll(PDO::FETCH_ASSOC);
