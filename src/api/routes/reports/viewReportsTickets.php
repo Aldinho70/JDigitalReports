29fa666627d0e0ad90227cd2f7c082b90a04c70b
Jesus Manuel Bobadilla <aldinhobobadilla@gmail.com>
2025-11-28 18:22:57 -0600

    Se agrega el endpoint para consultar la vista de reportes mas comentarios de soporte tecnico

---
+++ src/api/routes/reports/viewReportsTickets.php
@@ -0,0 +1,11 @@
+<?php
+require "../../core/db.php";
+require_once __DIR__ . "/../../config/cors.php";
+
+$db = new DB();
+$data = $db->query("SELECT * FROM `view_tickets_reportes`")->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($data);
+
+
+//http://localhost:8080/repos/Jornada%20Digital/JornadaDigital.ReportesDeUnidades.com/src/api/routes/reports/getReports.php
