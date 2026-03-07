4e2fc4b7040b6c665be1c77911a396b397a86bee
Jesus Manuel Bobadilla <aldinhobobadilla@gmail.com>
2026-03-07 14:22:53 -0600

    hotfix: Se corrige las respuestas de las rutas technician/

--- src/api/routes/technician/getCountJobTechnician.php
+++ src/api/routes/technician/getCountJobTechnician.php
@@ -6,16 +6,18 @@ header("Content-Type: application/json; charset=utf-8");
 
 $db = new DB();
 
-// Consulta dinámica segura
-$data = $db->query("SELECT 
-                            VRT.technician_name, 
-                            VRT.technician_phone, 
-                            VRT.technician_city, 
-                            VRT.technician_id, 
-                            COUNT(*) AS 'count_assignation'
-                        FROM 
-                            vw_reports_technicians AS VRT 
-                        GROUP BY VRT.technician_id;")->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($data);
+try {
+    $data = $db->query("SELECT 
+                                VRT.technician_name, 
+                                VRT.technician_phone, 
+                                VRT.technician_city, 
+                                VRT.technician_id, 
+                                COUNT(*) AS 'count_assignation'
+                            FROM 
+                                vw_reports_technicians AS VRT 
+                            GROUP BY VRT.technician_id;")->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode(["status" => "ok", "mensaje" => $data]);
+} catch (Exception $e) {
+    echo json_encode(["error" => $e->getMessage()]);
+}
 ?>
