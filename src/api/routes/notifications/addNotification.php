f3e6517125064fcde1fa0a8f965f3b5978d65cae
Jesus Manuel Bobadilla <aldinhobobadilla@gmail.com>
2026-07-13 14:19:56 -0600

    feat: Se agrega la peticion para las notificaciones desde wialon

---
+++ src/api/routes/notifications/addNotification.php
@@ -0,0 +1,26 @@
+<?php
+require "../../core/db.php";
+require_once __DIR__ . "/../../config/cors.php";
+
+$db = new DB();
+
+$body = json_decode(file_get_contents("php://input"), true);
+
+if (!$body) {
+    echo json_encode(["error" => "JSON invalido"]);
+    exit;
+}
+
+// Consulta SQL corregida
+$sql = "INSERT INTO `notification_test` (`id`, `notification`) 
+        VALUES (null, ?)";
+
+try {
+    $db->query($sql, [
+        $body["notification"],
+    ]);
+
+    echo json_encode(["status" => "ok"]);
+} catch (Exception $e) {
+    echo json_encode(["error" => $e->getMessage()]);
+}
